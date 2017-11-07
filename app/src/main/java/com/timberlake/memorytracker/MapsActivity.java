76538ff733a8dc3cfc3e4402d1d71634cf21675f
Mara Timberlake <maratimberlake@msn.com>
2017-11-06 16:36:24 -0800

    outline new methods and variables for MapsActivity.java

--- app/src/main/java/com/timberlake/memorytracker/MapsActivity.java
+++ app/src/main/java/com/timberlake/memorytracker/MapsActivity.java
@@ -14,8 +14,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
-    final static int PERMISSION_ALL = 1;
-    final static String[] PERMISSIONS = [android.Manifest.permission.ACCESS_COARSE_LOCATION];
+
     private GoogleMap mMap;
     LatLng myCoordinates;
     LocationManager locationManager;
@@ -29,6 +28,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+      mMap = googleMap;
+
+      // LatLng sydney = new LatLng(-34 151)
+      // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+      // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    @Override
+    public void onLocationChanged(Location location){
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s){
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s){
+
+    }
 
     private void requestLocation() {
         Criteria criteria = new Criteria();
