60a4e84ac536454327cc3617f79bcc72e6a32ff0
Mara Timberlake <maratimberlake@msn.com>
2017-11-06 16:49:53 -0800

    move map camera to new position

--- app/src/main/java/com/timberlake/memorytracker/MapsActivity.java
+++ app/src/main/java/com/timberlake/memorytracker/MapsActivity.java
@@ -15,53 +15,73 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
-    private GoogleMap mMap;
-    LatLng myCoordinates;
-    LocationManager locationManager;
+  final static int PERMISSION_ALL;
+  final static String() PERMISSIONS = {android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+  private GoogleMap mMap;
+  MarkerOptions mo;
+  Marker marker;
+  LocationManager locationManager;
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-      mMap = googleMap;
+  // LatLng myCoordinates;
+  // LocationManager locationManager;
 
-      // LatLng sydney = new LatLng(-34 151)
-      // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-      // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_maps);
+      SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+              .findFragmentById(R.id.map);
+      mapFragment.getMapAsync(this);
+      locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-    @Override
-    public void onLocationChanged(Location location){
+      mo = new MarkerOptions().position(new LatLng(0,0)).title("My Current Location");
 
-    }
+      if (Build.VERSION.SDK_INT >=23 && !isPermissionGranted()) {
+        requestPermissions(PERMISSIONS, PERMISSION_ALL);
+      } else requestLocation();
+      if(!isLocationEnabled()) {
+        showAlert(1);
+      }
+  }
 
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    mMap = googleMap;
+    marker mMap.addMarker(mo);
 
-    }
+    // LatLng sydney = new LatLng(-34 151)
+    // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+    // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+  }
 
-    @Override
-    public void onProviderEnabled(String s){
+  @Override
+  public void onLocationChanged(Location location){
+    LatLng myCoordinates = new LatLng(location.getLatitude(), location.getLongitude());
+    marker.setPosition(myCoordinates);
+    mMap.moveCamera(CameraUpdateFactory.newLatLng(myCoordinates));
+  }
 
-    }
+  @Override
+  public void onStatusChanged(String s, int i, Bundle bundle) {
 
-    @Override
-    public void onProviderDisabled(String s){
+  }
 
-    }
+  @Override
+  public void onProviderEnabled(String s){
 
-    private void requestLocation() {
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_FINE);
-        criteria.setPowerRequirement(Criteria.POWER_HIGH);
-        String provider = locationManager.getBestProvider(criteria, true);
-        locationManager.requestLocationUpdates(provider, 10000, 10, this);
-    }
+  }
+
+  @Override
+  public void onProviderDisabled(String s){
+
+  }
+
+  private void requestLocation() {
+      Criteria criteria = new Criteria();
+      criteria.setAccuracy(Criteria.ACCURACY_FINE);
+      criteria.setPowerRequirement(Criteria.POWER_HIGH);
+      String provider = locationManager.getBestProvider(criteria, true);
+      locationManager.requestLocationUpdates(provider, 10000, 10, this);
+  }
 }
